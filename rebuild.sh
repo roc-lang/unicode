f3579462a2c454b79f5a86b9c1ba4129fb14246a
Anton-4 <17049058+Anton-4@users.noreply.github.com>
2025-01-24 11:49:04 +0100

    just roc for rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo "Generating InternalEmoji.roc"
-../roc/target/release/roc package/InternalEmojiGen.roc > package/InternalEmoji.roc
+roc package/InternalEmojiGen.roc > package/InternalEmoji.roc
 
 echo "Generating InternalGBP.roc"
-../roc/target/release/roc package/InternalGBPGen.roc > package/InternalGBP.roc
+roc package/InternalGBPGen.roc > package/InternalGBP.roc
 
 echo "Generating GraphemeTest.roc"
-../roc/target/release/roc package/GraphemeTestGen.roc > package/GraphemeTest.roc
+roc package/GraphemeTestGen.roc > package/GraphemeTest.roc
 
 echo "Generating InternalEAW.roc"
-../roc/target/release/roc package/InternalEAWGen.roc > package/InternalEAW.roc
+roc package/InternalEAWGen.roc > package/InternalEAW.roc
